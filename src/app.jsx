ff1e2cb1b7bd5db66bcba33a673e7a9e50c837dd
pauleena phan <pauleena2002@gmail.com>
2024-02-05 15:35:43 -0800

    created form outline

--- src/app.jsx
+++ src/app.jsx
@@ -2,64 +2,88 @@ import { useState } from "react";
 
 
 export default function App(){
-    const[information, setInformation] = useState({
-        firstName: 'big bob',
-        lastName: '',
+    const[personalInfo, setPersonalInfo] = useState({
+        name: '',
         email: '',
         phoneNumber: ''
     });
 
-    function changeFirstName(e){
-        setInformation({
-            firstName: e.target.value,
-            lastName: information.lastName,
-            email: information.email,
-            phoneNumber: information.phoneNumber
+    function changeName(e){
+        setPersonalInfo({
+            name: e.target.value,
+            email: personalInfo.email,
+            phoneNumber: personalInfo.phoneNumber
         });
     }
 
-    function changeLastName(e){
-        setInformation({
-            firstName: information.firstName,
-            lastName: e.target.value,
-            email: information.email,
-            phoneNumber: information.phoneNumber
-        })
-    }
-
     function changeEmail(e){
-        setInformation({
-            firstName: information.firstName,
-            lastName: information.lastName,
+        setPersonalInfo({
+            name: personalInfo.name,
             email: e.target.value,
-            phoneNumber: information.phoneNumber
-        })
+            phoneNumber: personalInfo.phoneNumber
+        });
     }
 
     function changePhoneNum(e){
-        setInformation({
-            firstName: information.firstName,
-            lastName: information.lastName,
-            email: information.email,
+        setPersonalInfo({
+            name: personalInfo.name,
+            email: personalInfo.email,
             phoneNumber: e.target.value
-        })
+        });
     }
 
+    const[edInfo, setEdInfo] = useState({
+        institutionName: ''
+    });
+
+    function displayEd(e){
+        setEdInfo({
+            institutionName: document.getElementById("institutionName").value
+        });
+    }
 
     return (
         <>
             <h1>CV Application</h1>
-            <label>
-                {/* Label for the input box */}
-                First Name
-                {/* When the value of the input box is changed it calls the changefirst name function */}
-                <input onChange={changeFirstName} />
-            </label>
+            
+            {/* Personal Information */}
+            <div className = "personal_info">
+                <li>   
+                    <label>Name</label>
+                    <input type = "text" id = "name" onChange = {changeName}></input>
+                </li>
+                <li>
+                    <label>Email</label>
+                    <input type = "text" id = "email" onChange = {changeEmail}></input>
+                </li>
+                <li>
+                    <label>Number</label>
+                    <input type = "text" id = "phoneNumber" onChange = {changePhoneNum}></input>
+                </li>
+            </div>
+            
 
-
+            {/* Educational information */}
+            <form name="educationInfo" onSubmit={displayEd}>
+                <li>
+                    <label>Institution</label>
+                    <input type = "text" id = "institutionName"></input>
+                </li>
+                <li>
+                    <input type = "submit" value = "submit"></input>
+                </li>
+            </form>
+            
             <div className = "application">
-                {/* Prints the first name that is being changed  */}
-                {"first name:" + information.firstName}
+                <div className = "personalInfo">
+                    {/* Prints the first name that is being changed  */}
+                    {"First name:" + personalInfo.name}
+                    {"Email " + personalInfo.email}
+                    {"Phone number " + personalInfo.phoneNumber}
+                </div>
+                <div className = "educationInfo">
+                    {"Institution:" + edInfo.institutionName}
+                </div>
             </div>
         </>
     )
